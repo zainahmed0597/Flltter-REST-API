df1f0ff2fbd167dbc576af0d71ece67693cc2218
zainahmed0597 <zainahmed0597@gmail.com>
2023-01-06 16:07:06 +0500

    independent integration api successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rest_api/home_screen.dart';
 
+import 'example_two.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: const ExampleTwo(),
     );
   }
 }
