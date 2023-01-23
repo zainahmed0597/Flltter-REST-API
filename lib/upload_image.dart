2fd70230d8927950e5a9f264b255f48ac95d703e
zainahmed0597 <zainahmed0597@gmail.com>
2023-01-23 05:15:18 +0500

    Part-14 - Flutter Upload File/Image To Rest API/Server Using Multiport Http Request with Null Safety

--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class UploadImageScreen extends StatefulWidget {
   const UploadImageScreen({Key? key}) : super(key: key);
@@ -10,37 +11,101 @@ class UploadImageScreen extends StatefulWidget {
   State<UploadImageScreen> createState() => _UploadImageScreenState();
 }
 
-class _UploadImageScreenState extends State<UploadImageScreen>{
-
+class _UploadImageScreenState extends State<UploadImageScreen> {
   File? image;
   final _picker = ImagePicker();
   bool showSpinner = false;
 
+  Future getImage() async {
+    final pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+    if (pickedFile != null) {
+      image = File(pickedFile.path);
+      setState(() {});
+    } else {
+      print("no image selected");
+    }
+  }
+
+  Future<void> uploadImage() async {
+    setState(() {
+      showSpinner = true;
+    });
+    var stream = new http.ByteStream(image!.openRead());
+    stream.cast();
+    var length = await image!.length();
+    var uri = Uri.parse('https://fakestoreapi.com/products');
+    var request = new http.MultipartRequest('POST', uri);
+    request.fields['title'] = "Static =title";
+    var multiport = new http.MultipartFile('image', stream, length);
+    request.files.add(multiport);
+    var response = await request.send();
+    print(response.stream.toString());
+    if (response.statusCode == 200) {
+      setState(() {
+        showSpinner = false;
+      });
+      print('Image uploaded');
+    } else {
+      print('Upload image failed ');
+      setState(() {
+        showSpinner = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Upload Image'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            child: image == null ? const Center(child: Text("Image picker"),) :
-                Container(
-                  child: Center(
-                    child: Image.file(
-                      File(image!.path).absolute,
-                      height: 100,
-                      width: 100,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Upload Image'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                getImage();
+              },
+              child: Container(
+                child: image == null
+                    ? const Center(
+                        child: Text("Pick Image"),
+                      )
+                    : Container(
+                        child: Center(
+                          child: Image.file(
+                            File(image!.path).absolute,
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+              ),
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            GestureDetector(
+              onTap: () {
+                uploadImage();
+              },
+              child: Container(
+                height: 50,
+                width: 150,
+                color: Colors.green,
+                child: const Center(
+                  child: Text('Upload'),
                 ),
-          ),
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
